feb4e3b399e3445fa4fcf23f44cf8a0f72040dd4
Ansh Doshi <anshdoshi2305@gmail.com>
2021-12-11 15:58:42 +0530

    commit

---
+++ 6_Conditional Expressions/ad.py
@@ -0,0 +1,34 @@
+# Sometimes we want to play pubg on our phone if the day is Sunday.
+
+# Sometimes we order Ice-cream online if the day is sunny.
+
+# Sometimes we go hiking if our parents allow.
+
+# All these are decisions that depend on the condition being met.
+
+# In python programming too, we must be able to execute instructions on a condition(s) being met. This is what conditions are for!
+
+# If else and elif in Python
+# If else and elif statements are a multiway decision taken by our program due to certain conditions in our code.
+
+# Syntax:
+
+# '''
+# if (condition1):		// if condition 1 is true
+# 	print(“yes”)
+# elif (condition2):		// if condition 2 is true
+# 	print(“No”)
+# else:				// otherwise
+# 	print(“May be”)
+# '''
+
+
+a = 22
+if(a < 3):
+        print("less than 3")
+elif(a > 7):
+	print("greater than 7")
+
+else:
+    print("greter than 3 or less than or equal to 7")
+
